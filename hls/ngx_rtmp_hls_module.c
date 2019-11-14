f2024519dfc8ea7f8b10549bd1a04b027704dbc8
Chetan Dhembre <chetandhembre@gmail.com>
2019-11-14 22:06:27 +0530

    truncate file.

--- hls/ngx_rtmp_hls_module.c
+++ hls/ngx_rtmp_hls_module.c
@@ -621,8 +621,8 @@ ngx_rtmp_hls_write_playlist(ngx_rtmp_session_t *s)
     video_file_fd = ngx_open_file(video_file, NGX_FILE_APPEND,
                        NGX_FILE_OPEN, NGX_FILE_DEFAULT_ACCESS);
 
-    ngx_log_error(NGX_LOG_ERR, s->connection->log, ngx_errno,
-                      "hls failed: %u", video_file_fd);
+    // ngx_log_error(NGX_LOG_ERR, s->connection->log, ngx_errno,
+    //                   "hls failed: %u", video_file_fd);
         
     if (video_file_fd == NGX_INVALID_FILE) {
         video_file_fd = ngx_open_file(video_file, NGX_FILE_APPEND,
